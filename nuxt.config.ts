797acb040eb541de3ca9b78d2d18ef7b866f54ef
wobsoriano <sorianorobertc@gmail.com>
2022-11-30 10:49:57 -0800

    update deps

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,23 +1,9 @@
 import { telefunc } from 'telefunc/vite'
 
-import fs from 'fs'
-
 export default defineNuxtConfig({
-  build: {
-    // transpile: ['@prisma/client']
-  },
   vite: {
     plugins: [
       telefunc()
     ],
-    define: {
-      'process.env.DATABASE_URL': JSON.stringify(process.env.DATABASE_URL),
-    }
   },
-  hooks: {
-    'build:done'() {
-      console.log('finished building')
-    }
-  },
-  
 })
